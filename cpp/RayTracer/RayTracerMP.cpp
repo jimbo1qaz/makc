8c077277fe9d12f6770b4b1dc68132549eaef5ed
makc.the.great <makc.the.great@1eff5426-bb48-0410-838f-eba945681aae>
2010-01-15 16:24:25 +0000

    Lilu Dallas multipass

--- cpp/RayTracer/RayTracerMP.cpp
+++ cpp/RayTracer/RayTracerMP.cpp
@@ -152,8 +152,11 @@ struct Ray {
 	bool active;
 	int generation, s;
 	double magnitude, x, y, z, dx, dy, dz;
+	static void Clear (Ray *r) {
+		r->active = false; r->generation = 0; r->magnitude = 1; r->x = 0; r->y = 0; r->z = 0;
+	}
 	Ray::Ray () {
-		active = false; generation = 0; magnitude = 1; x = 0; y = 0; z = 0;
+		Clear (this);
 	}
 };
 
@@ -210,10 +213,10 @@ int main(int argc, char* argv[])
 
 	int const S = 512; // tracing SxS bitmap
 	int const N = 7; // rays per ray per generation
-	int const TTL = 2-1; // generations: rays per pixel should be < N^TTL
-/// constant step was causing crop circles :( 
-///	double const STEP = 0.002; // 0.0 < step << 1.0
-int const STEP_N = 12345;
+	int const TTL = 2; // generations: rays per pixel should be < N^TTL
+	double const STEP = 0.002; // 0.0 < STEP << 1.0
+	int const STEP_N = 12345; // number of different random steps to choose from
+	int const PASSES = 10;
 
 	int const MAX_RAYS = 10000000;
 
@@ -223,22 +226,16 @@ int const STEP_N = 12345;
 	int iHeight = S;
 	int iStride = STRIDE_3ALIGN4 (iWidth);
 	int iBufferLength = sizeof (BITMAPINFOHEADER) + iStride * iHeight;
-	unsigned char *pCanvasBitmap = new unsigned char [iBufferLength]; ZeroMemory (pCanvasBitmap, iBufferLength);
-
-	PBITMAPINFOHEADER pbih = (PBITMAPINFOHEADER) pCanvasBitmap; ZeroMemory (pbih, sizeof (BITMAPINFOHEADER));
-	pbih->biBitCount = 24; pbih->biCompression = BI_RGB; pbih->biHeight = iHeight; pbih->biWidth = iWidth;
-	pbih->biPlanes = 1; pbih->biSize = sizeof (BITMAPINFOHEADER); pbih->biSizeImage = iStride * pbih->biHeight;
-	pbih->biXPelsPerMeter = pbih->biYPelsPerMeter = 0;
-
-	unsigned char *canvas = (pCanvasBitmap + (WORD)(((PBITMAPINFOHEADER)pCanvasBitmap)->biSize));
+	unsigned char **pCanvasBitmap = new unsigned char *[PASSES];
 
 	// init arrays
-	double *red = new double [SS]; ZeroMemory (red, SS * sizeof (double));
-	double *green = new double [SS]; ZeroMemory (green, SS * sizeof (double));
-	double *blue = new double [SS]; ZeroMemory (blue, SS * sizeof (double));
-	int *num = new int [SS]; ZeroMemory (num, SS * sizeof (int));
+	double *red = new double [SS];
+	double *green = new double [SS];
+	double *blue = new double [SS];
+	int *num = new int [SS];
 	Ray *rays = new Ray [MAX_RAYS];
 	Ray *directions = new Ray [N];
+	double *steps = new double [STEP_N];
 
 	int i, j, k;
 
@@ -277,121 +274,170 @@ int const STEP_N = 12345;
 	double camSideY = camFwdZ * camUpX - camUpZ * camFwdX;
 	double camSideZ = camFwdX * camUpY - camUpX * camFwdY;
 
-	// generation zero
-	double fovAtan = 1.5; // 1 = 90 deg, infinity = 180 deg
-	for (i = 0; i < S; i++)
-	for (j = 0; j < S; j++) {
-		Ray &r = rays [i + S * j]; r.active = true;
-		r.s = i + S * j;
-		r.x = camPosX; r.dx = camFwdX + ((i - 0.5 * S) * camUpX + (j - 0.5 * S) * camSideX) * fovAtan / S;
-		r.y = camPosY; r.dy = camFwdY + ((i - 0.5 * S) * camUpY + (j - 0.5 * S) * camSideY) * fovAtan / S;
-		r.z = camPosZ; r.dz = camFwdZ + ((i - 0.5 * S) * camUpZ + (j - 0.5 * S) * camSideZ) * fovAtan / S;
-///		double rdL = STEP / sqrt (r.dx * r.dx + r.dy * r.dy + r.dz * r.dz);
-double rdL = 1 / sqrt (r.dx * r.dx + r.dy * r.dy + r.dz * r.dz);
-		r.dx *= rdL; r.dy *= rdL; r.dz *= rdL;
+	// output C++ code to override orientation
+	printf ("camPosX = %f;\n", camPosX);
+	printf ("camPosY = %f;\n", camPosY);
+	printf ("camPosZ = %f;\n", camPosZ);
+	printf ("camFwdX = %f;\n", camFwdX);
+	printf ("camFwdY = %f;\n", camFwdY);
+	printf ("camFwdZ = %f;\n", camFwdZ);
+	printf ("camUpX = %f;\n", camUpX);
+	printf ("camUpY = %f;\n", camUpY);
+	printf ("camUpZ = %f;\n", camUpZ);
+	printf ("camSideX = %f;\n", camSideX);
+	printf ("camSideY = %f;\n", camSideY);
+	printf ("camSideZ = %f;\n", camSideZ);
+
+	if (!false) {
+		// to set specific orientation
+		camPosX = -0.733089;
+		camPosY = 0.617209;
+		camPosZ = -0.285714;
+		camFwdX = 0.733089;
+		camFwdY = -0.617209;
+		camFwdZ = 0.285714;
+		camUpX = 0.000000;
+		camUpY = 0.420086;
+		camUpZ = 0.907484;
+		camSideX = -0.680132;
+		camSideY = -0.665267;
+		camSideZ = 0.307961;
 	}
 
-///	// pre-multiply scaterring directions
-///	for (k = 0; k < N; k++) {
-///		directions [k].dx *= STEP;
-///		directions [k].dy *= STEP;
-///		directions [k].dz *= STEP;
-///	}
-double *steps = new double [STEP_N];
-for (i = 0; i < STEP_N; i++) steps [i] = 0.002 * random ();
-int lastStep = 0;
+	// render several times
+	for (int pass = 0; pass < PASSES; pass++) {
+		pCanvasBitmap [pass] = new unsigned char [iBufferLength]; ZeroMemory (pCanvasBitmap [pass], iBufferLength);
 
+		PBITMAPINFOHEADER pbih = (PBITMAPINFOHEADER) pCanvasBitmap [pass]; ZeroMemory (pbih, sizeof (BITMAPINFOHEADER));
+		pbih->biBitCount = 24; pbih->biCompression = BI_RGB; pbih->biHeight = iHeight; pbih->biWidth = iWidth;
+		pbih->biPlanes = 1; pbih->biSize = sizeof (BITMAPINFOHEADER); pbih->biSizeImage = iStride * pbih->biHeight;
+		pbih->biXPelsPerMeter = pbih->biYPelsPerMeter = 0;
 
-	// loop
-	int beginAt = 0;
-	int newRaysAt = SS;
-	bool loop = true;
+		unsigned char *canvas = (pCanvasBitmap [pass] + (WORD)(((PBITMAPINFOHEADER)(pCanvasBitmap [pass]))->biSize));
 
-	while (loop) {
-		if (rays [beginAt].active) {
-			// process one ray
-			Ray &r = rays [beginAt];
+		// clean up arrays
+		ZeroMemory (red, SS * sizeof (double));
+		ZeroMemory (green, SS * sizeof (double));
+		ZeroMemory (blue, SS * sizeof (double));
+		ZeroMemory (num, SS * sizeof (int));
+		for (i = 0; i < MAX_RAYS; i++) Ray::Clear (&rays [i]);
 
-double step = steps [lastStep];
-lastStep++; lastStep %= STEP_N;
-///			r.x += r.dx; r.y += r.dy; r.z += r.dz;
-r.x += r.dx * step;
-r.y += r.dy * step;
-r.z += r.dz * step;
-			if (hitTest (r.x, r.y, r.z)) {
-				// we hit something - scatter
-				if (r.generation < TTL) {
-					for (int n1 = 0; n1 < N; n1++) {
-						Ray &r1 = rays [newRaysAt];
-						if (r1.active) {
-							puts ("\nRays pool exhausted :(\n");
-							break;
+		// generation zero
+		double fovAtan = 1.5; // 1 = 90 deg, infinity = 180 deg
+		for (i = 0; i < S; i++)
+		for (j = 0; j < S; j++) {
+			Ray &r = rays [i + S * j]; r.active = true;
+			r.s = i + S * j;
+			r.x = camPosX; r.dx = camFwdX + ((i - 0.5 * S) * camUpX + (j - 0.5 * S) * camSideX) * fovAtan / S;
+			r.y = camPosY; r.dy = camFwdY + ((i - 0.5 * S) * camUpY + (j - 0.5 * S) * camSideY) * fovAtan / S;
+			r.z = camPosZ; r.dz = camFwdZ + ((i - 0.5 * S) * camUpZ + (j - 0.5 * S) * camSideZ) * fovAtan / S;
+			double rdL = 1 / sqrt (r.dx * r.dx + r.dy * r.dy + r.dz * r.dz);
+			r.dx *= rdL; r.dy *= rdL; r.dz *= rdL;
+		}
+
+		// randomize (or else pass would be fully deterministic)
+		for (i = 0; i < STEP_N; i++) steps [i] = STEP * random ();
+		int lastStep = 0;
+
+		// loop
+		int beginAt = 0;
+		int newRaysAt = SS;
+		bool loop = true;
+
+		while (loop) {
+			if (rays [beginAt].active) {
+				// process one ray
+				Ray &r = rays [beginAt];
+
+				double step = steps [lastStep];
+				lastStep++; lastStep %= STEP_N;
+				r.x += r.dx * step; r.y += r.dy * step; r.z += r.dz * step;
+
+				if (hitTest (r.x, r.y, r.z)) {
+					// we hit something - scatter
+					if (r.generation < TTL) {
+						for (int n1 = 0; n1 < N; n1++) {
+							Ray &r1 = rays [newRaysAt];
+							if (r1.active) {
+								puts ("\nRays pool exhausted :(\n");
+								break;
+							}
+							r1.active = true;
+							r1.s = r.s;
+							r1.generation = r.generation + 1;
+							r1.x = r.x; r1.y = r.y; r1.z = r.z;
+							r1.dx = directions [n1].dx;
+							r1.dy = directions [n1].dy;
+							r1.dz = directions [n1].dz;
+							// this basically defines "material"
+							r1.magnitude = r.magnitude * 0.9;
+
+							newRaysAt++; newRaysAt %= MAX_RAYS;
 						}
-						r1.active = true;
-						r1.s = r.s;
-						r1.generation = r.generation + 1;
-						r1.x = r.x; r1.y = r.y; r1.z = r.z;
-						r1.dx = directions [n1].dx;
-						r1.dy = directions [n1].dy;
-						r1.dz = directions [n1].dz;
-						// this basically defines "material"
-						r1.magnitude = r.magnitude * 0.9;
-
-						newRaysAt++; newRaysAt %= MAX_RAYS;
 					}
-				}
-
-				r.active = false; beginAt++; beginAt %= MAX_RAYS;
-				if (beginAt % 12345 == 0) printf ("\rRay %d generation %d pool at %d        ", beginAt - 1, r.generation, newRaysAt);
-			} else {
-				double d2 = r.x * r.x + r.y * r.y + r.z * r.z;
-				if (d2 > 1.1) {
-					// we hit light - get color from lightmap
-					// this SHOULD be determined by the way lightmap is made ;)
-					d2 = 1 / sqrt (d2); r.x *= d2; r.y *= d2; r.z *= d2;
-					int lx = int (lmWidth * 0.25 * ( (r.z > 0) ? 1 + r.x : 3 - r.x ));
-						if (lx > lmWidth - 1) lx = lmWidth - 1; else if (lx < 0) lx = 0;
-					int ly = int (lmHeight * 0.5 * (1 + r.y));
-						if (ly > lmHeight - 1) ly = lmHeight - 1; else if (ly < 0) ly = 0;
-					Pixel *c = (Pixel *) &lightmap [lx * 3 + ly * lmStride];
-					red [r.s] += r.magnitude * c->R;
-					green [r.s] += r.magnitude * c->G;
-					blue [r.s] += r.magnitude * c->B;
-					num [r.s] += 1;
 
 					r.active = false; beginAt++; beginAt %= MAX_RAYS;
-					if (beginAt % 12345 == 0) printf ("\rRay %d generation %d pool at %d light  ", beginAt - 1, r.generation, newRaysAt);
+					if (beginAt % 12345 == 0) printf ("\rPass %d ray %d generation %d pool at %d        ", pass, beginAt - 1, r.generation, newRaysAt);
+				} else {
+					double d2 = r.x * r.x + r.y * r.y + r.z * r.z;
+					if (d2 > 1.1) {
+						// we hit light - get color from lightmap
+						// this SHOULD be determined by the way lightmap is made ;)
+						d2 = 1 / sqrt (d2); r.x *= d2; r.y *= d2; r.z *= d2;
+						int lx = int (lmWidth * 0.25 * ( (r.z > 0) ? 1 + r.x : 3 - r.x ));
+							if (lx > lmWidth - 1) lx = lmWidth - 1; else if (lx < 0) lx = 0;
+						int ly = int (lmHeight * 0.5 * (1 + r.y));
+							if (ly > lmHeight - 1) ly = lmHeight - 1; else if (ly < 0) ly = 0;
+						Pixel *c = (Pixel *) &lightmap [lx * 3 + ly * lmStride];
+						red [r.s] += r.magnitude * c->R;
+						green [r.s] += r.magnitude * c->G;
+						blue [r.s] += r.magnitude * c->B;
+						num [r.s] += 1;
+
+						r.active = false; beginAt++; beginAt %= MAX_RAYS;
+						if (beginAt % 12345 == 0) printf ("\rPass %d ray %d generation %d pool at %d light  ", pass, beginAt - 1, r.generation, newRaysAt);
+					}
 				}
+			} else {
+				// no more rays left
+				loop = false;
 			}
-		} else {
-			// no more rays left
-			loop = false;
 		}
+
+		// save the image
+		int s; double sum = 1;
+		for (s = 0; s < SS; s++) sum += num [s]; sum /= SS;
+		
+		for (s = 0; s < SS; s++) {
+			// ugly hack for sky :(
+			double ns = (num [s] > 1) ? /* body */ sum : /* sky: */ 1;
+			int ir = int (red   [s] / ns); if (ir > 255) ir = 255;
+			int ig = int (green [s] / ns); if (ig > 255) ig = 255;
+			int ib = int (blue  [s] / ns); if (ib > 255) ib = 255;
+			Pixel *c = (Pixel *) &canvas [(s % S) * 3 + (s / S) * iStride];
+			c->R = unsigned char (ir);
+			c->G = unsigned char (ig);
+			c->B = unsigned char (ib);
+		}
+
+		char framename [100]; sprintf (framename, "frame%02d.bmp", pass);
+		saveBitmap (pCanvasBitmap [pass], framename);
 	}
 
-	// save the image
-	int s; double sum = 1;
-	for (s = 0; s < SS; s++) sum += num [s]; sum /= SS;
-	
-	for (s = 0; s < SS; s++) {
-		// ugly hack for sky :(
-		double ns = (num [s] > 1) ? /* body */ sum : /* sky: */ 1;
-		int ir = int (red   [s] / ns); if (ir > 255) ir = 255;
-		int ig = int (green [s] / ns); if (ig > 255) ig = 255;
-		int ib = int (blue  [s] / ns); if (ib > 255) ib = 255;
-		Pixel *c = (Pixel *) &canvas [(s % S) * 3 + (s / S) * iStride];
-		c->R = unsigned char (ir);
-		c->G = unsigned char (ig);
-		c->B = unsigned char (ib);
+	// make composite bitmap
+	for (i = 0; i < SS; i++) {
+		k = (i % S) * 3 + (i / S) * iStride;
+		int ir = 0, ig = 0, ib = 0; Pixel *c;
+		for (j = 0; j < PASSES; j++) {
+			c = (Pixel *) &(pCanvasBitmap [j] [k]);
+			ir += c->R; ig += c->G; ib += c->B;
+		}
+		c = (Pixel *) &(pCanvasBitmap [0] [k]);
+		c->R = unsigned char (ir / PASSES);
+		c->G = unsigned char (ig / PASSES);
+		c->B = unsigned char (ib / PASSES);
 	}
-
-	saveBitmap (pCanvasBitmap, "render.bmp");
-
-
-
-
-
-
+	saveBitmap (pCanvasBitmap [0], "composite.bmp");
 
 	// release stuff
 	delete [] steps;
@@ -401,6 +447,8 @@ r.z += r.dz * step;
 	delete [] blue;
 	delete [] green;
 	delete [] red;
+	for (i = 0; i < PASSES; i++)
+		delete [] pCanvasBitmap [i];
 	delete [] pCanvasBitmap;
 	delete [] pLightmapBitmap;
 	Gdiplus::GdiplusShutdown(gdiplusToken);
